2a0cf8043997c1e14a287652832b62349352fac7
Juraj Jancuska <jjancuska@gmail.com>
2011-03-25 23:14:28 +0100

    Hook Image helper in plugin bootstrap

--- config/nodeattachment_bootstrap.php
+++ config/nodeattachment_bootstrap.php
@@ -1,5 +1,6 @@
 <?php
         Croogo::hookBehavior('Node', 'Nodeattachment.Nodeattachment');
+        Croogo::hookHelper('*', 'Image');
         Croogo::hookAdminTab('Nodes/admin_edit', 'Attachments', 'nodeattachment.admin_tab_node');
         Croogo::hookAdminMenu('Nodeattachment');
 ?>
