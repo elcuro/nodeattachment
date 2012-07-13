1891e4a3725e2ac46b8b2d0f1d08b9f42ab5692d
Juraj Jancuska <jjancuska@gmail.com>
2012-07-14 00:19:17 +0200

    removed duplicated hooking of image2 helper

--- config/nodeattachment_bootstrap.php
+++ config/nodeattachment_bootstrap.php
@@ -14,7 +14,6 @@
         Configure::write('Nodeattachment.thumbnailExt', 'png');
 
         Croogo::hookBehavior('Node', 'Nodeattachment.Nodeattachment');
-        Croogo::hookHelper('*', 'Image2');
         Croogo::hookHelper('*', 'Nodeattachment.Nodeattachment');
         Croogo::hookAdminTab('Nodes/admin_edit', 'Attachments', 'nodeattachment.admin_tab_node');
         Croogo::hookAdminMenu('Nodeattachment');
