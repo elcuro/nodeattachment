4e08925b6ceeea72a29a7a3d971fa2bbf091af5d
Juraj Jancuska <jjancuska@gmail.com>
2012-08-13 12:48:21 +0200

    horizontal admin menu created by CroogoNav

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -10,11 +10,22 @@
             'thumbExt' => 'png'
         );
         Configure::write('Nodeattachment', Set::merge($tmp_conf, $conf));
-
         Configure::write('Nodeattachment.thumbnailExt', 'png');
 
         Croogo::hookBehavior('Node', 'Nodeattachment.Nodeattachment');
         Croogo::hookHelper('*', 'Nodeattachment.Nodeattachment');
+        
+        CroogoNav::add('extensions.children.nodeattachment', array(
+            'title' => 'Nodeattachment',
+            'url' => '#',
+            'children' => array(
+                'settings' => array(
+                    'title' => __('Configure'),
+                    'url' => array('plugin' => '', 'controller' => 'settings', 'action' => 'prefix', 'Nodeattachment')
+                )
+            )
+        ));
+        
         Croogo::hookAdminTab('Nodes/admin_edit', 'Attachments', 'Nodeattachment.admin_tab_node');
         Croogo::hookAdminMenu('Nodeattachment');
 ?>
--- View/Elements/admin_menu.ctp
+++
@@ -1,4 +0,0 @@
-<a href="#">Nodeattachment</a>
-<ul>
-        <li><?php echo $html->link(__('Configure', true), array('plugin' => '', 'controller' => 'settings', 'action' => 'prefix', 'Nodeattachment')); ?></li>
-</ul>
